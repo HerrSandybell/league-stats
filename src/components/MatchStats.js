e912496063e5d6ab5cff19d4f4d515b87529e28c
Tarek <talatrach@gmail.com>
2022-09-20 01:48:24 -0600

    Last tweaks

--- src/components/MatchStats.js
+++ src/components/MatchStats.js
@@ -10,14 +10,11 @@ function MatchStats (props) {
 
     const displayGameEndTime = (gameEnd) => {
         const today = new Date().getTime();
-
         const timeDiff = today - gameEnd;
-
         const timeDiffDays = timeDiff / (1000 * 60 * 60 * 24);
 
         if (timeDiffDays < 2 && timeDiff >= 1) {
             return `Yesterday`;
-
         } else if (timeDiffDays > 2) {
             return `${Math.floor(timeDiffDays)} days ago`;
         } else {
@@ -42,15 +39,12 @@ function MatchStats (props) {
                     {displayDuration(props.match.duration)}
                 </div>
             </div>
-            <div>
-                <div>
-                    {props.match.gameMode}
-                </div>
-                <div>
-                    {props.match.summonerspells.map(spell => <div>{spell}</div>)}
-                </div>
-                <div>
+            <div className={styles['champion-info']}>
+                <div className={styles['champion']}>
                     {props.match.champion.name}
+                </div>
+                <div className={styles['summoner-spells']}>
+                    {props.match.summonerspells.map(spell => <div>{spell}</div>)}
                 </div>
             </div>
             <div>
